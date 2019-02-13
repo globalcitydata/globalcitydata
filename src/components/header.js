cd4a09ffd9ab9f6655ef0a92407b738c8640e672
Rafael Barash <rafabarash@gmail.com>
2019-02-13 14:52:11 -0600

    another small edit for dana

--- src/components/header.js
+++ src/components/header.js
@@ -11,11 +11,11 @@ export default () => (
   <>
     <header style={{ marginBottom: '1.5rem' }}>
       <Link to="/" style={{ textShadow: 'none', backgroundImage: 'none' }}>
-        <h3 style={{ display: 'inline' }}>GlobalCityData</h3>
+        <h3 style={{ display: 'inline' }}>Global City Data</h3>
       </Link>
       <ul style={{ listStyle: 'none', float: 'right' }}>
         <ListLink to="/">Home</ListLink>
-        <ListLink to="/story/">Story</ListLink>
+        <ListLink to="/story/">Our Story</ListLink>
         <ListLink to="/contact/">Contact</ListLink>
       </ul>
     </header>
