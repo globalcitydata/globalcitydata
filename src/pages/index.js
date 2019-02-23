a983048d147ab8e3619903b7542b1bf882edbfa5
Rafael Barash <rafabarash@gmail.com>
2019-02-23 17:45:56 -0600

    added basic fluid image to homepage. need to make responsive eventually. can do this later after getting all queries working and implementing search

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,73 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import style from 'styled-components';
 
 // Components
+import { Carousel } from 'grommet';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Container from '../components/container';
+import { ContainedButton } from '../components/buttons';
+
+const Showcase = style.section`
+  position: relative;
+  color: #fff;
+  text-align: center;
+`;
+
+const ImagesWrapper = style.div`
+  position: absolute;
+  width: 100%;
+  max-height: 650px;
+  z-index: -1;
+  filter: brightness(40%);
+
+  @media (max-width: 767px) {
+    height: 1500px;
+  }
+`;
+
+const ContentWrapper = style.div`
+  color: white;
+  padding: 30px 5% 0;
+  max-width: 1000px;
+  margin: 0 auto;
+  text-align: center;
+
+  @media (max-width: 767px) {
+    padding: 25px 20px;
+  }
+`;
+
+const WelcomeImages = ({ images }) => (
+  <ImagesWrapper>
+    <Carousel fill play="6000">
+      {images.map(img => (
+        <Img fluid={img.fluid} />
+      ))}
+    </Carousel>
+  </ImagesWrapper>
+);
 
 export default ({ data }) => {
-  const { primaryMessage, secondaryMessage } = data.contentfulHomePage;
+  const {
+    primaryMessage,
+    secondaryMessage,
+    welcomeImages,
+  } = data.contentfulHomePage;
   return (
     <>
       <SEO title="Home" />
       <Layout>
-        <Container>
-          <h1>{primaryMessage}</h1>
-          <p>{secondaryMessage}</p>
-        </Container>
+        <Showcase>
+          <WelcomeImages images={welcomeImages} />
+          <ContentWrapper>
+            <h1 style={{ color: '#fff' }}>{primaryMessage}</h1>
+            <p>{secondaryMessage}</p>
+            <ContainedButton label="Explore Data" href="/data/" />
+          </ContentWrapper>
+        </Showcase>
       </Layout>
     </>
   );
@@ -24,6 +76,11 @@ export default ({ data }) => {
 export const query = graphql`
   query {
     contentfulHomePage {
+      welcomeImages {
+        fluid(maxHeight: 500, maxWidth: 1500) {
+          ...GatsbyContentfulFluid_withWebp
+        }
+      }
       primaryMessage
       secondaryMessage
     }
