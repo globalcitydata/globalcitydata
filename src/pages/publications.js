4fa76360d4b5e38d9e1cb88e0397db833c95c3f8
Rafael Barash <rafabarash@gmail.com>
2019-02-23 17:03:11 -0600

    updated publications to link to related data

--- src/pages/publications.js
+++ src/pages/publications.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 
 // Components
 import Layout from '../components/layout';
@@ -15,13 +15,16 @@ export default ({ data }) => {
         <Container>
           <h1>Publications</h1>
           {publications.map((pub) => {
-            const { url, body: bodyOutside } = pub.node;
-            const { body } = bodyOutside;
+            // const { url } = pub.node;
+            const { body } = pub.node.body;
+            const { slug } = pub.node.relatedData;
+
             return (
               <div key={body}>
-                <a href={url}>{body}</a>
-                <br />
-                <br />
+                <p>
+                  {body}
+                  <Link to={`/data/${slug}`}>Associated Data</Link>
+                </p>
               </div>
             );
           })}
@@ -40,8 +43,65 @@ export const query = graphql`
           body {
             body
           }
+          relatedData {
+            slug
+          }
         }
       }
     }
   }
 `;
+
+// import React from 'react';
+// import { graphql, Link } from 'gatsby';
+
+// // Components
+// import { Heading, Text } from 'grommet';
+// import Container from '../components/container';
+// import Layout from '../components/layout';
+// import SEO from '../components/seo';
+
+// export default ({ data }) => {
+//   const publications = data.allContentfulPublication.edges;
+//   return (
+//     <Layout>
+//       <SEO title="Publications" />
+//       <Container>
+//         <Heading>Publications</Heading>
+//         {publications.map((pub) => {
+//           // const { url } = pub.node;
+//           const { body } = pub.node.body;
+//           const { slug } = pub.node.relatedData;
+
+//           return (
+//             <div key={body}>
+//               <Text>{body}</Text>
+//               <Link to={`/data/${slug}`}>Associated Data</Link>
+
+//               <br />
+//               <br />
+//             </div>
+//           );
+//         })}
+//       </Container>
+//     </Layout>
+//   );
+// };
+
+// export const query = graphql`
+//   query {
+//     allContentfulPublication {
+//       edges {
+//         node {
+//           url
+//           body {
+//             body
+//           }
+//           relatedData {
+//             slug
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
