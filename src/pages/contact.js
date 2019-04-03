0c11fc68e224a5dce0f50172c84d5bab2778c946
Rafael Barash <rafabarash@gmail.com>
2019-04-03 13:12:42 -0500

    feat #17: added hero background/img behind page titles

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,19 +3,21 @@ import React from 'react';
 // Components
 import { Typography } from '@material-ui/core';
 import Container from '../components/container';
+import Hero from '../components/hero';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import withRoot from '../withRoot';
 
 const Contact = () => (
-  <>
+  <Layout>
     <SEO title="Contact" />
-    <Layout>
-      <Container>
-        <Typography variant="h3">Contact us</Typography>
-      </Container>
-    </Layout>
-  </>
+    <Hero title="Contact us" />
+    <Container>
+      <Typography variant="body2">
+        Eventually let Dana edit from Contentful... Also add contact form?
+      </Typography>
+    </Container>
+  </Layout>
 );
 
 export default withRoot(Contact);
--- src/pages/publications.js
+++ src/pages/publications.js
@@ -1,23 +1,20 @@
 import React from 'react';
-
-// Components
-import { Typography } from '@material-ui/core';
-import Layout from '../components/layout';
-import SEO from '../components/seo';
-import Container from '../components/container';
-import PublicationSearch from '../components/publications/publicationSearch';
 import withRoot from '../withRoot';
+import SEO from '../components/seo';
+import Layout from '../components/layout';
+import Container from '../components/container';
+import Hero from '../components/hero';
+import PublicationSearch from '../components/publications/publicationSearch';
 
 const Publications = () => (
-  <>
+  <Layout>
     <SEO title="Publications" />
-    <Layout>
-      <Container>
-        <Typography variant="h3">Publications</Typography>
-        <PublicationSearch />
-      </Container>
-    </Layout>
-  </>
+    <Hero title="Publications" />
+    <Container>
+      {/* <Typography variant="h3">Publications</Typography> */}
+      <PublicationSearch />
+    </Container>
+  </Layout>
 );
 
 export default withRoot(Publications);
