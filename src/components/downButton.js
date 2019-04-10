a481941882450aed00107be5908e26f8dbd63a4a
Rafael Barash <rafabarash@gmail.com>
2019-04-10 13:12:00 -0500

    small changes to look of homepage

--- src/components/downButton.js
+++ src/components/downButton.js
@@ -1,32 +1,25 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import { Button, Fab } from '@material-ui/core';
-import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import { IconButton } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { Link, animateScroll as scroll } from 'react-scroll';
 
-const styles = {
-  fab: {
-    marginTop: '1rem',
-  },
-};
+const styles = {};
 
 const DownButton = ({ id, classes }) => (
   <div>
-    <Fab
+    <IconButton
       component={Link}
       to={id}
       spy
       smooth
-      // hashSpy
       offset={-50}
       duration={700}
-      // delay={500}
-      color="secondary"
+      color="white"
       aria-label="Down"
-      className={`${classes.fab} lift`}
     >
-      <ArrowDownwardIcon />
-    </Fab>
+      <ExpandMoreIcon />
+    </IconButton>
   </div>
 );
 
