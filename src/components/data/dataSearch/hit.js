b42cda9bcf548ddb27085c3cb819a988c318a3ce
Rafael Barash <rafabarash@gmail.com>
2019-04-10 10:52:07 -0500

    feat #23: data cards with expansion panels. they look too similar and quite ugly. will try to change to previews

--- src/components/data/dataSearch/hit.js
+++ src/components/data/dataSearch/hit.js
@@ -1,100 +1,89 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import { Link } from 'gatsby';
 import { withStyles } from '@material-ui/core/styles';
 
 // General Components
 import {
   Grid,
-  Divider,
   Button,
   Typography,
   Card,
   CardContent,
   CardActions,
   Chip,
-  Collapse,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails,
 } from '@material-ui/core';
-
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { getDataObjFromHit } from '../dataUtils';
 
-const styles = {
-  card: {
-    height: '100%',
-    position: 'relative',
+const styles = theme => ({
+  ep: {
+    margin: 0,
+    borderRadius: 'inherit',
   },
-  title: {
-    marginBottom: '1rem',
-  },
-  content: {
-    paddingTop: '1rem',
-    paddingBottom: '4rem',
+  epDetail: {
+    display: 'inherit',
   },
   chip: {
     margin: '0 10px 10px 0',
+  },
+  actionWrapper: {
+    paddingTop: '4rem',
   },
   action: {
     position: 'absolute',
     bottom: '0.6rem',
   },
-};
+});
 
 const colors = ['primary', 'secondary', 'default'];
 
-const Tags = ({ classes, tags }) => {
-  const [expanded, setExpanded] = useState(true);
-  return (
-    <div>
-      {/* <Collapse expanded={expanded.toString()}> */}
-      <>
-        {Object.entries(tags).map((tag, i) => (
-          <div key={i}>
-            {Object.entries(tag[1]).map(attribute => (
-              <Chip
-                color={colors[i % colors.length]}
-                label={attribute[1]}
-                className={classes.chip}
-                key={attribute[1]}
-              />
-            ))}
-          </div>
-        ))}
-        {/* <Chip label="Example tag" className={classes.chip} />
-    <Chip color="primary" label="Another one" className={classes.chip} />
-    <Chip color="secondary" label="one more baby" /> */}
-        {/* </Collapse> */}
-      </>
-    </div>
-  );
-};
+const Tags = ({ classes, tags }) => (
+  <ExpansionPanel className={classes.ep}>
+    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+      <Typography variant="subtitle1">Tags</Typography>
+    </ExpansionPanelSummary>
+    <ExpansionPanelDetails className={classes.epDetail}>
+      {Object.entries(tags).map((tag, i) => (
+        <Fragment key={i}>
+          {Object.entries(tag[1]).map(attribute => (
+            <Chip
+              color={colors[i % colors.length]}
+              label={attribute[1]}
+              className={classes.chip}
+              key={attribute[1]}
+            />
+          ))}
+        </Fragment>
+      ))}
+    </ExpansionPanelDetails>
+  </ExpansionPanel>
+);
 
-const Summary = ({ classes, summary }) => {
-  const [expanded, setExpanded] = useState(false);
-  return (
-    <div>
-      <Collapse expanded={expanded.toString()}>
-        <Typography variant="body2">{summary}</Typography>
-      </Collapse>
-    </div>
-  );
-};
+const Summary = ({ classes, summary }) => (
+  <ExpansionPanel className={classes.ep}>
+    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+      <Typography variant="subtitle1">Summary</Typography>
+    </ExpansionPanelSummary>
+    <ExpansionPanelDetails className={classes.epDetail}>
+      <Typography paragraph>{summary}</Typography>
+    </ExpansionPanelDetails>
+  </ExpansionPanel>
+);
 
 const Hit = ({ hit, classes }) => {
   const dataObj = getDataObjFromHit(hit);
-  // console.log(dataObj);
   return (
     <Grid item xs={12} md={6}>
       <Card className={`${classes.card} lift`}>
         <CardContent>
-          <Typography variant="h6" className={classes.title}>
-            {dataObj.title}
-          </Typography>
-          <Divider />
-          <div className={classes.content}>
-            <Tags tags={dataObj.tags} classes={classes} />
-            <Summary summary={dataObj.summary} classes={classes} />
-          </div>
+          <Typography variant="h6">{dataObj.title}</Typography>
         </CardContent>
-        <CardActions>
+        <Summary summary={dataObj.summary} classes={classes} />
+        <Tags tags={dataObj.tags} classes={classes} />
+        <CardActions className={classes.actionWrapper}>
           <Button
             color="primary"
             component={Link}
